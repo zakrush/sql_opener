172d9a5759bd60c03a21fb33640850bc0d2f6fbf
zakrush <zakrush@yandex.ru>
2020-08-03 13:08:21 +0300

    add split for output

--- validator/valid_checkers.py
+++ validator/valid_checkers.py
@@ -10,7 +10,7 @@ def find_pattern(response):
 
 
 def url_validator(url):
-    res = re.match(r'http(s)?://(www.)?[a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)', url)
+    res = re.match(r'http(s)?://(www.)?[a-zA-Z0-9@:%._+~#=\-]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)', url)
     if res:
         return res.group()
     else:
